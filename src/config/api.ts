08dd7749ac88f1af533b1abd74a312b52dda8ac6
FlynnRider-000 <coronarider01@gmail.com>
2021-09-19 23:58:05 +0300

    api url update

--- src/config/api.ts
+++ src/config/api.ts
@@ -1,2 +1,2 @@
-// export const apiUrl = 'https://api.beta.musselfarm.co.nz/';
-export const apiUrl = 'http://10.0.2.2/';
+export const apiUrl = 'https://api.beta.musselfarm.co.nz/';
+// export const apiUrl = 'http://10.0.2.2/';
